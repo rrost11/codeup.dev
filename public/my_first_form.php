648570436b242640d28d7e54c7df71878edb5acc
Rachel Rost <rachel.v.rost@gmail.com>
2016-01-27 11:44:23 -0600

    git push origin master

---
+++ public/my_first_form.php
@@ -0,0 +1,29 @@
+
+
+<?php
+  var_dump($_POST);
+?>
+<title>My First Form</title>
+
+<form method="POST" action="/My First HTML Form">
+    <p>
+        <label for="username">Username</label>
+        <input id="username" name="username" type="text">
+    </p>
+    <p>
+        <label for="password">Password</label>
+        <input id="password" name="password" type="password">
+    </p>
+    <p>
+        <input type="submit">
+    </p>
+    <p>
+    <input type="text" id="username" name="username" placeholder="Enter your username"></p>
+    <label for="username">Username:</label>
+<input type="text" id="username" name="username">
+
+</form>	
+
+<form><input type="checkbox" id="mailing_list" name="mailing_list" value="yes">
+<label for="mailing_list">Sign me up for the mailing list!</label>
+</form>
