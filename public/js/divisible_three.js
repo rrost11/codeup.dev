a16212924df4feec890a2fe7b754a4b4303b5976
Rachel Rost <rachel.v.rost@gmail.com>
2016-02-20 15:22:07 -0600

    git commit

--- public/js/divisible_three.js
+++ public/js/divisible_three.js
@@ -1,8 +1,8 @@
 
-function getNumbersDivisibleByThree(arr){
+function getNumbersDivisibleByThree(array){
 	var result = []
-	for(var i = 0; i < arr.length; i++){
-		var val = arr[i]
+	for(var i = 0; i < array.length; i++){
+		var val = array[i]
 		if(Math.floor(val/3) === val/3){
 			result.push(val)
 		}
@@ -11,8 +11,8 @@ function getNumbersDivisibleByThree(arr){
 }
 
 
-var arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22];
+var array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22];
 
-var result = getNumbersDivisibleByThree(arr);
+var result = getNumbersDivisibleByThree(array);
 
 console.log(result)
