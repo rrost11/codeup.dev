232b7531c76bf9cd870ff40a49ef3ec38143476c
Rachel Rost <rachel.v.rost@gmail.com>
2016-08-01 15:18:37 -0500

    Made Function Class Exercise Code More Clear and Efficient

--- public/js/function_exercise.js
+++ public/js/function_exercise.js
@@ -1,6 +1,36 @@
 'use strict';
 
+// Exercise #1
+var averageGrade = (70 + 80 + 95)/3;
 
+if(averageGrade > 80) {
+    console.log("You're awesome!");
+} else {
+    console.log("You need to practice more!");
+}
+
+
+function gradeStudent(grade1, grade2, grade3) {
+    var averageGrade = (grade1 + grade2 + grade3) / 3;
+    var message;
+    if(averageGrade > 80) {
+    message = "You're awesome!";
+} else {
+    message ="You need to practice more!";
+}
+    return message;
+}
+
+console.log(gradeStudent(75,90,80));
+console.log(gradeStudent(25,100,70));
+console.log(gradeStudent(80,85,78));
+
+
+
+
+
+
+// Exercise #2
 
 
 function payment(person, salesAmount) {
@@ -8,28 +38,54 @@ function payment(person, salesAmount) {
 
     var discount = 0.35;
     var finalAmount;
+    var message;
 
     if (salesAmount > 200) {
 
         finalAmount = salesAmount - (salesAmount * discount);
 
-        console.log(person + "'s purhcases totaled $" + salesAmount + " and a 35% discount was applied.  " + person + "'s final payment is: $" + finalAmount);
+        message = person + "'s purhcases totaled $" + salesAmount + " and a 35% discount was applied.  " + person + "'s final payment is: $" + finalAmount;
+        
 
     } else {
         finalAmount = salesAmount;
-        console.log(person + "'s purchases totaled $" + salesAmount + " and no discount was applied.  " + person + "'s final payment is: $" + finalAmount);
+        message = person + "'s purchases totaled $" + salesAmount + " and no discount was applied.  " + person + "'s final payment is: $" + finalAmount;
+        
     }
+        return message;
 }
 
 
-payment("Ryan", 180);
+console.log(payment("Ryan", 180));
+
+console.log(payment("Cameron", 250));
+
+console.log(payment("George", 320));
 
 
 
-payment("Cameron", 250);
 
 
+// Exercise #3
 
-payment("George", 320);
+
+function carOrHouse(){
+
+    var flipACoin = Math.floor(Math.random() * 2);
+    var message; 
+
+if (flipACoin === 0) {
+    message ='Isaac, you should buy a car!';
+} else {
+    message = 'Isaac, you should buy a house!';
+}
+    return message;
+}
+
+console.log(carOrHouse());
+console.log(carOrHouse());
+console.log(carOrHouse());
+console.log(carOrHouse());
+console.log(carOrHouse());
 
 
