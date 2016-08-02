fc51f6ff058cb403263f689345e335e22a427875
Rachel Rost <rachel.v.rost@gmail.com>
2016-08-02 09:49:08 -0500

    JavaScript User Interaction Class Exercise

---
+++ public/js/user-interaction.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var name = prompt("Please enter your name: ");
+
+	if(name = ""){
+		prompt("Please enter your name: ");
+	} else {
+		alert("Welcome" + name);
+	}
+
+var pizzaLike = prompt("Do you like pizza?");
+	
+		
+
+
+
+// TODO: Ask the user for their name.
+//       Keep asking if an empty input is provided.
+
+// TODO: Show an alert message that welcomes the user based on their input.
+
+// TODO: Ask the user if they like pizza.
+//       Based on their answer show a creative alert message.
