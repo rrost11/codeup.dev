1d722585ce023b3482c5bcd1f10216c625dd16ff
Rachel Rost <rachel.v.rost@gmail.com>
2016-08-02 13:51:03 -0500

    Lecture Notes & Examples on Arrays

---
+++ public/js/array_lecture.js
@@ -0,0 +1,46 @@
+'use strict';
+
+// declare and initialize array
+var shapes = ['square','rectangle','circle','triangle'];
+
+// declare an empty array
+var emptyArray = [];
+
+
+
+// declare and initialize array
+var shapes = new Array('square', 'rectangle', 'circle', 'triangle');
+
+// declare an empty array
+var emptyArray = new Array();
+
+
+
+// declare and initialize array
+var shapes = ['square', 'rectangle', 'circle', 'triangle'];
+
+// log the number of items in the array
+console.log(shapes.length); // 4
+
+
+
+// declare and initialize array
+var shapes = ['square', 'rectangle', 'circle', 'triangle'];
+
+// There are 4 shapes in the array
+console.log('There are ' + shapes.length + ' shapes in the array');
+
+// The first shape is: square
+console.log('The first shape is: ' + shapes[0]);
+
+// The second shape is: rectangle
+console.log('The second shape is: ' + shapes[1]);
+
+// The third shape is: circle
+console.log('The third shape is: ' + shapes[2]);
+
+// The fourth shape is: triangle
+console.log('The fourth shape is: ' + shapes[3]);
+
+// The fifth shape is: undefined
+console.log('The fifth shape is: ' + shapes[4]);
