1ff8efd356100fb4129e8acdbb26a753e2c1c099
Rachel Rost <rachel.v.rost@gmail.com>
2016-08-03 10:53:57 -0500

    Added comments and made code easier to read and understand

--- public/js/planets-array.js
+++ public/js/planets-array.js
@@ -1,87 +1,110 @@
+// TODO: Create an array holding the names of the eight planets 
+// in our solar system in order, starting closest to the sun.
+
 (function() {
     "use strict";
 
-    // TODO: Create an array holding the names of the eight planets in our solar system in order, starting closest to the sun.
     var planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"];
 
 
-    // function for logging the planets array
+//  ---------------This is the function for logging the planets array-----------------------
+    
     function logPlanets() {
         console.log("Here is the list of planets:");
         console.log(planets);
         console.log("---- ---- ---- ----");
     }
+
+// ---------------------------------Add The Sun---------------------------------------------
     
     logPlanets();
-
+    
     console.log('Adding "The Sun" to the beginning of the planets array.');
-    
-        planets.unshift("The Sun");
 
-        logPlanets();
+    planets.unshift("The Sun");
 
+    logPlanets();
 
-    
+// -------------------------------Add Pluto--------------------------------------------------
+
     console.log('Adding "Pluto" to the end of the planets array.');
 
-        planets.push("Pluto");
+    planets.push("Pluto");
 
-        logPlanets();
-        
+    logPlanets();
 
+// -----------------------------Remove The Sun------------------------------------------------
 
     console.log('Removing "The Sun" from the beginning of the planets array.');
 
-        planets.shift("The Sun");
+    planets.shift("The Sun");
 
-        logPlanets();
-    
+    logPlanets();
+
+// ------------------------------Remove Pluto--------------------------------------------------
 
     console.log('Removing "Pluto" from the end of the planets array.');
-    
-        planets.pop();
-    
-        logPlanets();
-    
+
+    planets.pop();
+
+    logPlanets();
+
+// --------------------------------Find & Log Earth--------------------------------------------
+
     console.log('Finding and logging the index of "Earth" in the planets array.');
-    
+
     console.log(planets.indexOf("Earth"));
     logPlanets();
-    
-    console.log('Using splice to remove the planet after Earth.');
-    
-        planets.splice(planets.indexOf("Earth") + 1, 1);
-    
-        logPlanets();
-    
-    console.log('Using splice to add back the planet after Earth.');
-    
-        planets.splice(planets.indexOf("Earth") + 1, 0, "Mars");
-    
-        logPlanets();
-    
-    console.log('Reversing the order of the planets array.');
-    
-        planets.reverse();
-    
-        logPlanets();
-    
-    console.log('Sorting the planets array.');
-    
-        planets.sort();
-    
-        logPlanets();
-    
-    // todo: Join the planets array with pipes (|) to create a variable named 'planetsAsString'.
-    var planetsAsString = planets.join('|');
-    
-    console.log(planetsAsString);
-    // todo: Split the 'planetsAsString' variable by pipes (|) to create a variable named 'planetsAsArray'.
-    
-    var planetsAsArray = planetsAsString.split('|');
-    
-    console.log(planetsAsArray);
-    
 
-})()
-;
+// --------------------------------Remove Earth with Splice------------------------------------
+
+    console.log('Using splice to remove the planet after Earth.');
+
+    planets.splice(planets.indexOf("Earth") + 1, 1);
+
+    logPlanets();
+
+// ---------------------------------Use Splice to Add Earth Back-------------------------------
+
+    console.log('Using splice to add back the planet after Earth.');
+
+    planets.splice(planets.indexOf("Earth") + 1, 0, "Mars");
+
+    logPlanets();
+
+// ---------------------------------Reverse the Order of Planets-------------------------------
+
+    console.log('Reversing the order of the planets array.');
+
+    planets.reverse();
+
+    logPlanets();
+
+// -----------------------------------Sorting The Planets--------------------------------------
+
+    console.log('Sorting the planets array.');
+
+    planets.sort();
+
+    logPlanets();
+
+// ------------------------------Join The Planet with Pipes------------------------------------
+
+    // todo: Join the planets array with pipes (|) to create a variable named 'planetsAsString'.
+
+    var planetsAsString = planets.join('|');
+
+    console.log(planetsAsString);
+
+
+// ------------------------------Split the Variable with Pipes---------------------------------
+
+
+    // todo: Split the 'planetsAsString' variable by pipes (|) to create a variable named 'planetsAsArray'.
+
+    var planetsAsArray = planetsAsString.split('|');
+
+    console.log(planetsAsArray);
+
+
+})();
