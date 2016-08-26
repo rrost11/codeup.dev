78e9a7c37e9b980e0038c58eab6d839df4b9ef92
Rachel Rost <rachel.v.rost@gmail.com>
2016-08-26 16:48:26 -0500

    Cleaned up and beautified code

--- public/form.php
+++ public/form.php
@@ -3,6 +3,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <title>Form Debugging Exercise</title>
 </head>
 <body>
