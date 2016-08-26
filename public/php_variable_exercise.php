48788b5a76d327548f3a6b599c3aa9f65cb0ae7d
Rachel Rost <rachel.v.rost@gmail.com>
2016-08-26 16:42:04 -0500

    Intro PHP Exercise

---
+++ public/php_variable_exercise.php
@@ -0,0 +1,65 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>PHP Variabe Exercise</title>
+</head>
+<body>
+<? php > echo 455 * 875;
+398125
+php > echo 398125 - 43125;
+355000
+php > echo 355000 + 645000;
+1000000
+php > echo 1000000/25;
+40000
+php > $greeting = 'Hello Codeup!';
+php > echo $greeting;
+Hello Codeup!php > $answer = 5 + 4;
+php > echo $answer;
+9
+php > $a = 10;
+php > $b = 20;
+php > $c = $b / $a;
+php > echo $c;
+2
+php > $result = 4 % 3;
+php > echo $result;
+1
+php > $result = 5 % 3;
+php > echo $result;
+2
+php > $result = 6 % 3;
+php > echo $result;
+0
+php > $result = 7 % 3;
+php > echo $result;
+1
+php > $result = 8 % 3;
+php > echo $result;
+2
+php > $result = 6 % 4;
+php > echo $result;
+2
+php >
+php > $pi = pi();
+php > echo $pi;
+3.1415926535898
+php > echo $pi . PHP_EOL;
+3.1415926535898
+php > var_dump(true);
+bool(true)
+php > var_dump($result);
+int(2)
+php > $result = 1999;
+php > var_dump($result);
+int(1999)
+php > $result = pi();
+php > var_dump($result);
+double(3.1415926535898)
+php > $result = true;
+php > var_dump($result);
+bool(true)
+php ?>
+</body>
+
+</html>
