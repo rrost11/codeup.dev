39e5ea24765908f9697ad9da26953f8012ce5809
Rachel Rost <rachel.v.rost@gmail.com>
2016-09-09 13:45:02 -0500

    PHP Counter Class Exercise

---
+++ public/my-favorite-things.php
@@ -0,0 +1,27 @@
+<!-- We are going to build a simple list of your favorite things and display these in the form of a table in HTML.
+
+Create a new file named my-favorite-things.php and put it in your public directory.
+Create an array of your favorite things. This array should contain at least five things.
+We will need to loop through this array and display each element inside your HTML.
+Use an HTML table for this display.
+Use CSS to add a light gray background on every other row for a nice effect.
+Have fun and make it beautiful! -->
+
+<?php
+
+$myfavorites = ['Shelby the Beagle', 'Cheese Enchiladas', 'San Antonio Spurs', 'Chas', 'Traveling', 'Houston Texans'];
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>My Favorite Things</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	
+</body>
+</html>
