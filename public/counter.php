a62a9a9c0420dd8f7f4d10d21549553574a9faed
Rachel Rost <rachel.v.rost@gmail.com>
2016-09-13 15:12:01 -0500

    Cleaned up code

--- public/counter.php
+++ public/counter.php
@@ -3,12 +3,28 @@
 function pageController() {
 	$data = [];
 
+
+
+	// old way 
+	if(isset($_GET['count'])) {
+		$data['counter'] = $_GET['counter'];
+	} else {
+		$data['counter'] = 0;
+	}
+
+
+	// new way
 	if(inputHas('counter')) {
 		$data['counter'] = inputGet('counter');
 	} else {
-		
+		$data['counter'] = 0;
 	}
 }
+
+$data['up'] = $data['counter'] + 1;
+$data['down'] = $data['counter'] - 1;
+
+return data;
 
 
 
