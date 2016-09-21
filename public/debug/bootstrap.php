f9fdf7e550a7c13e02b984a183c4279cacc5f443
Rachel Rost <rachel.v.rost@gmail.com>
2016-09-21 09:52:52 -0500

    Updated Debug Exercise Solution

---
+++ public/debug/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once __DIR__ . '/lib/Log.php';
+require_once __DIR__ . '/lib/Auth.php';
+require_once __DIR__ . '/lib/Input.php';
+require_once __DIR__ . '/lib/Request.php';
+
+session_start();
