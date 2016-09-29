31320d7deafdd83593866425474c8755e563726c
Rachel Rost <rachel.v.rost@gmail.com>
2016-09-28 20:21:17 -0500

    Connected MySQL database using PDO by creating a new PDO object

--- public/pdo_test.php
+++ public/pdo_test.php
@@ -1,9 +1,8 @@
 <?php
 define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'parks_db');
+define('DB_NAME', 'nastional_parks');
 define('DB_USER', 'parks_user');
 define('DB_PASS', 'Els3vier');
 require 'db_connect.php';
 echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . PHP_EOL;
 
-?>
