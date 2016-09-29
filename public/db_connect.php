7dcb05ea60f4edda6f76a985344b6d13357a2b46
Rachel Rost <rachel.v.rost@gmail.com>
2016-09-28 20:12:49 -0500

    SQL & PHP Scripts connecting database

--- public/db_connect.php
+++ public/db_connect.php
@@ -1,7 +1,7 @@
 <?PHP
 
 // Get new instance of PDO object
-$dbc = new PDO('mysql:host=127.0.0.1;dbname=parks_db', 'parks_user', 'Els3vier');
+$dbc = new PDO('mysql:host=127.0.0.1;dbname=national_parks', 'parks_user', 'Els3vier');
 
 // Tell PDO to throw exceptions on error
 $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
