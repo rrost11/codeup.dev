10169d13ef6bc7b9d56849c85c9be803ef244817
Rachel Rost <rachel.v.rost@gmail.com>
2016-09-28 20:11:53 -0500

    Completed PHP Ping and Pong Exercise

---
+++ public/pong.php
@@ -0,0 +1,35 @@
+<?php 
+
+require_once 'Input.php';
+
+function pageController()
+{
+    $counter = Input::get('counter', 0);
+    $action  = Input::get('action', '');
+
+    return [
+        'counter' => $counter,
+        'action'  => $action
+    ];
+}
+
+extract(pageController());
+
+?>
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <title>Pong</title>
+     <link rel="stylesheet" href="css/style.css">
+ </head>
+ <body>
+     <h1>Pong</h1>
+    <?php if ($action !== ''): ?>
+        <h2><?= $action; ?></h2>
+    <?php endif; ?>
+     <h2>Counter: <?= $counter;?></h2>
+     <a href="ping.php?counter=<?= $counter+1; ?>&action=hit">HIT</a>
+     <a href="ping.php?counter=0&action=miss">Miss</a>
+ </body>
+ </html>
